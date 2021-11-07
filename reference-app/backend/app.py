8b888a36619530ed72aec208e253382368c4584d
Prabor Mukherjee <prabor1@gmail.com>
2021-11-07 17:07:37 +0530

    fixed bug

--- reference-app/backend/app.py
+++ reference-app/backend/app.py
@@ -40,9 +40,6 @@ CORS(app)
 jaeger_tracer = config_tracer()
 tracing = FlaskTracing(jaeger_tracer, True, app)
 
-
-app = Flask(__name__)
-
 app.config['MONGO_DBNAME'] = 'example-mongodb'
 app.config['MONGO_URI'] = 'mongodb://example-mongodb-svc.default.svc.cluster.local:27017/example-mongodb'
 
